e850ef5c207efc6500fb0fade3d5982523e5d557
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-30 21:33:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  results = data.each_with_object({}) do |(keys, value), array|
+  results = data.each_with_object({}) do |(key, value), array|
     value.each do |inside_key, names|
       names.each do |name|
         if !array[name]
           array[name] = {}
         end
-        if !array[name][keys]
-          !array[name][keys] = []
+        if !array[name][key]
+          !array[name][key] = []
         end
-        array[name][keys].push(inside_key.to_s)
+        array[name][key].push(inside_key.to_s)
       end
     end
   end
