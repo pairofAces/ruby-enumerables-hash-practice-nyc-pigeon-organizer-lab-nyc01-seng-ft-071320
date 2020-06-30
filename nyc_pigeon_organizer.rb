9179c9fcb11a2e90138cf8d7762d819be35579cd
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-30 21:46:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,21 +3,11 @@ def nyc_pigeon_organizer(data)
   results = data.each_with_object({}) do |(key, value), array|
     value.each do |inside_key, names|
       names.each do |name|
-        
-    
-
-end
-  results = data.each_with_object({}) do |(key, value), array|
-    value.each do |inside_key, names|
-      names.each do |name|
         if !array[name]
           array[name] = {}
         end
         if !array[name][key]
-          !array[name][key] = []
-        end
-        array[name][key].push(inside_key.to_s)
-      end
-    end
-  end
+          
+    
+
 end
